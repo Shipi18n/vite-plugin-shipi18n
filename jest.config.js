5785473fad7d984b5b919127beec90e74fad6ae2
owgreen-dev <ogreenowow@gmail.com>
2025-11-27 04:12:20 -0800

    test: update Jest config to collect coverage from source files
    - Add collectCoverageFrom to include src/**/*.js
    - Add coverage reporters (text, lcov, clover)
    - Exclude test files and node_modules from coverage

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,16 @@ export default {
   moduleFileExtensions: ['js', 'mjs'],
   testMatch: ['**/__tests__/**/*.test.js', '**/*.test.js'],
   verbose: true,
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!**/__tests__/**',
+    '!**/node_modules/**'
+  ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/',
+    '/coverage/'
+  ],
+  coverageReporters: ['text', 'lcov', 'clover'],
 }
